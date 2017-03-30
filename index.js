b8214e1253eea880c5ba1f87436224238f847756
Stephen P Rosso <stephenprosso@Stephens-MacBook-Pro.local>
2017-03-30 16:17:52 -0700

    first Commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
-  let message = {
-    username: 'Cameron',
-    message: 'Looks like somebody is running the sample!'
-  };
-
-  $.ajax('http://originmessages.herokuapp.com/messages', message, function(data) {
-
-  })
-)};
+$('#register').on('submit', function(ev) {
+   ev.preventDefault();
+   let message = {
+       username: $('#name').val(),
+       message: $('#message').val()
+   };
+   
+   $.ajax({
+       type: 'POST',
+       url: 'http://originmessages.herokuapp.com/messages',
+       data: JSON.stringify(message),
+       contentType: "application/json",
+       dataType: "json"
+   });
+   return false;
+});
